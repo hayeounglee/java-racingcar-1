ce9bbe5aa772ed3caa8791241c1605e1b511911a
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-08-11 14:56:09 +0900

    test: 시도 회수가 0이하일 때 재입력을 요구한다.

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -115,4 +115,22 @@ public class RacingGameTest {
         assertTrue(output.contains("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분)."));
         assertTrue(output.contains("이름은 5자를 초과하거나 공백이거나 null 일 수 없습니다."));
     }
+
+    @Test
+    @DisplayName("시도 회수가 0이하일 때 재입력을 요구한다.")
+    void testTryCount(){
+        String input = "0\n1\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+
+        view.InputView.getTryCount();
+
+        String output = outContent.toString();
+        assertTrue(output.contains("0이하는 입력할 수 없습니다."));
+    }
+
+
 }
