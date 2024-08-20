f7de25277431d9b3bdf715acbdaf9ce505030857
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-08-20 23:51:11 +0900

    refactor: 자동차 이름 확인하는 로직을 자동차 객체로 옮김

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,11 +1,16 @@
 package domain;
 
 public class Car {
+    private static final int NAME_LENGTH_MAXIMUM = 5;
+    private static final String NAME_VALIDATION_ERROR_1 = "이름은 " + NAME_LENGTH_MAXIMUM + "자를 초과할 수 없습니다.";
+
+    private static final String NAME_VALIDATION_ERROR_2 = "이름은 null 이거나 공백일 수 없습니다.";
     private String name;
     private int distance;
     private static final int MOVE_THRESHOLD = 4;
 
     public Car(String name){
+        validateName(name);
         this.name = name;
         this.distance = 0;
     }
@@ -25,4 +30,14 @@ public class Car {
     public void incrementDistance(){
         this.distance = this.distance + 1;
     }
+
+    private void validateName(String name){
+        if(name.length() > NAME_LENGTH_MAXIMUM){
+            throw new IllegalArgumentException(NAME_VALIDATION_ERROR_1);
+        }
+        if(name==null || name.isBlank()){
+            throw new IllegalArgumentException(NAME_VALIDATION_ERROR_2);
+        }
+    }
 }
+
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,9 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static final int MAX_CAR_NAME_LENGTH = 5;
     public static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
-    public static final String NAME_VALIDATION_ERROR = "이름은 " + MAX_CAR_NAME_LENGTH + "자를 초과하거나 공백이거나 null 일 수 없습니다.";
     public static final String TRY_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
     public static final String TRY_COUNT_ERROR = "0이하는 입력할 수 없습니다.";
     private Scanner scanner = new Scanner(System.in);
@@ -16,29 +14,18 @@ public class InputView {
     public List<String> getCarNames(){
         while (true) {
             System.out.println(CAR_NAME_PROMPT);
-
             String input = scanner.nextLine();
-            List<String> carNames = Arrays.asList(input.split(","));
-
-            if (isValidName(carNames)) return carNames;
+           return Arrays.asList(input.split(","));
         }
-    }
-
-    private boolean isValidName(List<String> carNames){
-        for(String name : carNames){
-            if(name.length() > MAX_CAR_NAME_LENGTH || name.trim().isEmpty() || name.length() == 0) {
-                System.out.println(NAME_VALIDATION_ERROR);
-                return false;
-            }
-        }
-        return true;
     }
 
     public int getTryCount(){
         while(true) {
             System.out.println(TRY_COUNT_PROMPT);
             int tryCount = scanner.nextInt();
-            if(tryCount > 0) return tryCount;
+            if(tryCount > 0) {
+                return tryCount;
+            }
             System.out.println(TRY_COUNT_ERROR);
         }
     }
