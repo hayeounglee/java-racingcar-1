ec0b8edde62ef760f4ac4989b8a7e3d950670a08
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-08-21 22:12:30 +0900

    Test: RacingGame 한 라운드 실행 시, 숫자가 4 이상일 때만 자동차가 전진한다

--- src/test/java/Domain/RacingGameTest.java
+++ src/test/java/Domain/RacingGameTest.java
@@ -4,6 +4,9 @@ import domain.Car;
 import domain.RacingGame;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import strategy.FixedNumberGenerator;
+import strategy.NumberGenerator;
+import strategy.RandomNumberGenerator;
 import view.InputView;
 
 
@@ -20,15 +23,14 @@ public class RacingGameTest {
     @Test
     @DisplayName("RacingGame 한 라운드 실행 시, 숫자가 4 이상일 때만 자동차가 전진한다")
     void moveWhenNumberIs_GreaterThanFour(){
-        RacingGame racingGame = new RacingGame(List.of("car1, car2"), 1);
+        FixedNumberGenerator fixedNumberGenerator = new FixedNumberGenerator(List.of(3,4));
+        RacingGame racingGame = new RacingGame(fixedNumberGenerator, List.of("car1, car2"), 1);
 
         racingGame.playGame();
 
         List<Car> racingCars = racingGame.getRacingCars();
         assertEquals(racingCars.get(0).getDistance(), 0);
         assertEquals(racingCars.get(1).getDistance(), 1);
-
-
     }
 
 }
