c9fba4773f04221923dbaded6cfd09f8762b9638
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-08-21 22:36:07 +0900

    refactor: 방어적 복사로 수정

--- src/main/java/strategy/FixedNumberGenerator.java
+++ src/main/java/strategy/FixedNumberGenerator.java
@@ -1,11 +1,12 @@
 package strategy;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FixedNumberGenerator implements NumberGenerator{
     private final List<Integer> numbers;
     public FixedNumberGenerator(List<Integer> numbers){
-        this.numbers = List.copyOf(numbers);
+        this.numbers = new ArrayList<>(numbers);
     }
     @Override
     public int generator() {
