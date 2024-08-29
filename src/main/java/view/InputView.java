7ab8eb0490b7f16fd3b34dd8680f0230d55099e6
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-08-29 22:30:56 +0900

    refactor: final 사용

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -9,7 +9,7 @@ public class InputView {
     public static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     public static final String TRY_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
     public static final String TRY_COUNT_ERROR = "0이하는 입력할 수 없습니다.";
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner = new Scanner(System.in);
 
     public List<String> getCarNames(){
         System.out.println(CAR_NAME_PROMPT);
