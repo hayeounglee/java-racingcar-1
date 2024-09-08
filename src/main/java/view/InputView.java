b6fd303ee007132391ce3b2f17bf1969f8f9988e
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-09-08 21:26:54 +0900

    refactor: 싱글톤의 동시성 문제 해결하기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
-    private static InputView inputView;
     private static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TRY_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
     private final Scanner scanner = new Scanner(System.in);
@@ -15,10 +14,11 @@ public class InputView {
     }
 
     public static InputView getInstance(){
-        if (inputView == null){
-            inputView = new InputView();
-        }
-        return inputView;
+        return LazyHolder.inputView;
+    }
+
+    private static class LazyHolder {
+        private static final InputView inputView = new InputView();
     }
 
     public List<String> getCarNames(){
