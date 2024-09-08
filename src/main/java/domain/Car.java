d8e0855f13a411431474fa6c0d4d37628fddbd6e
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-09-08 15:05:52 +0900

    refactor: 의미없는 숫자 상수로 만들지 않기

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,8 +2,6 @@ package domain;
 
 public class Car {
     private static final int NAME_LENGTH_MAXIMUM = 5;
-    private static final String NAME_VALIDATION_ERROR_1 = "이름은 " + NAME_LENGTH_MAXIMUM + "자를 초과할 수 없습니다.";
-    private static final String NAME_VALIDATION_ERROR_2 = "이름은 null 이거나 공백일 수 없습니다.";
     private static final int MOVE_THRESHOLD = 4;
 
     private String name;
@@ -17,10 +15,10 @@ public class Car {
 
     private void validateName(String name) {
         if (name == null || name.isBlank()) {
-            throw new IllegalArgumentException(NAME_VALIDATION_ERROR_2);
+            throw new IllegalArgumentException("이름은 null 이거나 공백일 수 없습니다.");
         }
         if (name.length() > NAME_LENGTH_MAXIMUM) {
-            throw new IllegalArgumentException(NAME_VALIDATION_ERROR_1);
+            throw new IllegalArgumentException("이름은 " + NAME_LENGTH_MAXIMUM + "자를 초과할 수 없습니다.");
         }
     }
 
