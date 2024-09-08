0951d2c1d767ed6147d00ddb76c60f819968664e
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-09-08 22:46:52 +0900

    refactor: 공백 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 public class InputView {
     private static final String CAR_NAME_PROMPT = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
     private static final String TRY_COUNT_PROMPT = "시도할 회수는 몇회인가요?";
+
     private final Scanner scanner = new Scanner(System.in);
 
     private InputView() {
